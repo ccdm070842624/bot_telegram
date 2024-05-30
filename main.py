2c0e24e65b21b25a46eb4ccf4ab2369995db8f4d
ccdm070842624 <ilja2000danlichev@gmail.com>
2024-05-30 07:03:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-token = telebot.telebot('7036229459:AAGSdT9O3DPrv0lv4L5mDSAhu0nvMDqMuk0')
+bot = telebot.telebot('7036229459:AAGSdT9O3DPrv0lv4L5mDSAhu0nvMDqMuk0')
 
 @bot.message.handler(commands=['start'])
 def main(message):
