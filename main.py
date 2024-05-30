0c5351e449467ad4300173af764b92fce6fa3c51
ccdm070842624 <ilja2000danlichev@gmail.com>
2024-05-30 07:00:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import telebot
 token = telebot.telebot('7036229459:AAGSdT9O3DPrv0lv4L5mDSAhu0nvMDqMuk0')
+
+@bot.message.handlers(commands=['start'])
+def main(message):
+  bot.send_message(message.chat.id, 'Hello')
