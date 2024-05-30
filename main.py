add49307687cd80b7f5712297f47dedba0b9009a
ccdm070842624 <ilja2000danlichev@gmail.com>
2024-05-30 07:06:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ bot = telebot.telebot('7036229459:AAGSdT9O3DPrv0lv4L5mDSAhu0nvMDqMuk0')
 @bot.message.handler(commands=['start'])
 def main(message):
   bot.send_message(message.chat.id, 'Hello')
+
+
+bot.polling(non_stop=True)
